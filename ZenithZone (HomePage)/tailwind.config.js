50010eb840b6776db489d4ec9fa5a029b39129c4
Rajib Kumar <rajib2516@student.nstu.edu.bd>
2024-12-03 03:31:19 +0600

    Remove unnecessary files

--- ZenithZone (HomePage)/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import daisyui from "./node_modules/daisyui";
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-};
